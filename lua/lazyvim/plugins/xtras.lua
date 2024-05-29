1e7a8e7c25dbbeab3b6d738889cbd81f18261e34
pix <tpxsky@163.com>
2024-05-30 06:48:02 +0800

    update xtras.lua

--- lua/lazyvim/plugins/xtras.lua
+++ lua/lazyvim/plugins/xtras.lua
@@ -3,10 +3,15 @@ local Config = require("lazyvim.config")
 -- Some extras need to be loaded before others
 local prios = {
   ["lazyvim.plugins.extras.editor.aerial"] = 100,
-  ["lazyvim.plugins.extras.editor.symbols-outline"] = 100,
+  ["lazyvim.plugins.extras.editor.outline"] = 100,
+  ["lazyvim.plugins.extras.editor.trouble-v3"] = 100,
+  ["lazyvim.plugins.extras.ui.edgy"] = 2,
   ["lazyvim.plugins.extras.test.core"] = 1,
   ["lazyvim.plugins.extras.dap.core"] = 1,
 }
+
+---@type string[]
+Config.json.data.extras = LazyVim.dedup(Config.json.data.extras)
 
 table.sort(Config.json.data.extras, function(a, b)
   local pa = prios[a] or 10
